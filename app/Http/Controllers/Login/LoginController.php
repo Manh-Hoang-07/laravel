60802e12350e447df54e8859d669a83109839941
Manh-Hoang-07 <vumanhhoang140799@gmail.com>
2022-08-19 23:58:05 +0700

    update category 19/08/2022

---
+++ app/Http/Controllers/Login/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Login;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login() {
+        if(auth()->check()) {
+            return redirect()->route('categories.index');
+        }
+        return view('login.login');
+    }
+
+    public function login_handling(Request $request) {
+        $remember = $request->has('remember');
+        if(auth()->attempt(
+            [
+                'email' => $request->email ?? '',
+                'password' => $request->password ?? ''
+            ], $remember
+        )) {
+            toastr()->success('Đăng nhập thành công');
+            return redirect()->route('categories.index');
+        }
+        toastr()->error('Tài khoản hoặc mật khẩu không chính xác');
+        return redirect()->route('login');
+    }
+}
